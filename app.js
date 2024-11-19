e3997ddd4ae82b1bab7a416b45c6499b2ba1b8cd
Jason <jtimm@gicw.org>
2024-11-19 13:48:45 -0800

    fixed js errors

--- app.js
+++ app.js
@@ -1,79 +1,72 @@
 'use strict';
 
-function CookieStore (store, customerMin, customerMax, averageCookie, cookieEachHour, avgCookiesPerSale) {
-    this.locationName= store,
-    this.customerMin= customerMin,
-    this.customerMax= customerMax,
-    this.avgCookiesPerSale = avgCookiesPerSale;
-    this.averageCookie=averageCookie,
-    this.cookieEachHour=cookieEachHour,
-    this.customersEachHour = [];
-    this.cookiesEachHour = [];
-    this.totalDailyCookies = 0;
-    }
-
-    // CookieStore (locationName, customerMin, customerMax, averageCookie, cookieEachHour, avgCookiesPerSale);
-    CookieStore();
-
-CookieStore.prototype.calCustomerEachHour= function () {// creates a prototype, will have a for loop
-    for (let i=0; i< hours.length;i++) { // i is the index starting at 0; the condition says that if i is less than number of hours it is true (will stop when it is false); finally the index will be incremented up by 1
-        this.customersEachHour.push (random(this.customerMin, this.customerMax)); // the code on this line uses a the method `this`for customerEachHour for all locations data about this. dot push adds the number to the end of the array
-
-    } 
-
-}
-
-
-
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-const store = [seattle, paris, lima, dubai, tokyo]; // Updated array name to ''
 
 function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function estimateSales(store) {
-    const sales = [];
-    for (let i = 0; i < hours.length; i++) {
-        const numCustomers = random(store.customerMin, store.customerMax);
-        const hourSales = Math.ceil(numCustomers * store.averageCookie);
-        sales.push(hourSales);// adds hourSales to the end of the array sales []
-    }
-    return sales;
+function CookieStore(store, customerMin, customerMax, averageCookie) {
+    this.locationName = store;
+    this.customerMin = customerMin;
+    this.customerMax = customerMax;
+    this.averageCookie = averageCookie;
+    this.customersEachHour = [];
+    this.cookiesEachHour = []; // Fixed typo (was cookieEachHour)
+    this.totalDailyCookies = 0;
 }
 
-function render(store) {
-    let total = 0;
+CookieStore.prototype.calCustomersEachHour = function () {
+    for (let i = 0; i < hours.length; i++) {
+        this.customersEachHour.push(random(this.customerMin, this.customerMax));
+    }
+};
+
+CookieStore.prototype.calCookiesEachHour = function () {
+    this.calCustomersEachHour();
+    for (let i = 0; i < hours.length; i++) {
+        const hourlyCookies = Math.ceil(this.customersEachHour[i] * this.averageCookie);
+        this.cookiesEachHour.push(hourlyCookies); // Fixed typo (was cookieEachHour)
+        this.totalDailyCookies += hourlyCookies;
+    }
+};
+
+CookieStore.prototype.render = function () {
     const root = document.getElementById('root');
     const location = document.createElement('section');
-   //  location.classList.add('location');
     root.appendChild(location);
 
     const title = document.createElement('h2');
-    title.textContent = this.store.name;
+    title.textContent = this.locationName;
     location.appendChild(title);
 
     const list = document.createElement('ul');
     location.appendChild(list);
 
     for (let i = 0; i < hours.length; i++) {
-        total += store.cookieEachHour[i];
         const listItem = document.createElement('li');
-        listItem.textContent = hours[i] + ': ' + store.cookieEachHour[i] + ' cookies';
+        listItem.textContent = `${hours[i]}: ${this.cookiesEachHour[i]} cookies`; // Fixed typo (was cookieEachHour)
         list.appendChild(listItem);
     }
 
     const totalItem = document.createElement('li');
-    totalItem.textContent = 'Total: ' + total + ' cookies';
+    totalItem.textContent = `Total: ${this.totalDailyCookies} cookies`;
     list.appendChild(totalItem);
-}
+};
+
+const seattle = new CookieStore('Seattle', 25, 42, 13);
+const paris = new CookieStore('Paris', 25, 42, 13);
+const lima = new CookieStore('Lima', 25, 42, 13);
+const dubai = new CookieStore('Dubai', 25, 42, 13);
+const tokyo = new CookieStore('Tokyo', 25, 42, 13);
+
+const stores = [seattle, paris, lima, dubai, tokyo];
 
 function runApplication() {
-    for (let i = 0; i < store.length; i++) {
-        store[i].estimate();
-        render(store[i]);
+    for (let i = 0; i < stores.length; i++) {
+        stores[i].calCookiesEachHour();
+        stores[i].render(); // Added parentheses to call the render method
     }
 }
 
-seattle.render();
 runApplication();
